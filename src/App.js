48c815e82e8e012577e37afe699e2f2ec10d0cc4
Joe Hebert <raphaelhebert18@gmail.com>
2021-07-30 11:12:08 +0700

    stretch

--- src/App.js
+++ src/App.js
@@ -6,28 +6,44 @@ import data from './data';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+import ProductContext from './contexts/ProductContext';
+import CartContext from './contexts/CartContext';
 
 function App() {
 	const [products] = useState(data);
-	const [cart, setCart] = useState([]);
+	const local = JSON.parse(localStorage.getItem('lambdaCart') || '[]')
+	const [cart, setCart] = useState(local);
+	// localStorage.setItem('lambdaCart', cart)
 
 	const addItem = item => {
 		setCart([ ...cart, item])
+		localStorage.setItem('lambdaCart', JSON.stringify([ ...cart, item]))
 	};
 
+	const removeItem = id => {
+		setCart(cart.filter(item => item.id !== id))
+		localStorage.setItem('lambdaCart', JSON.stringify(cart.filter(item => item.id !== id)))
+	}
+
 	return (
-		<div className="App">
-			<Navigation cart={cart} />
+		
+			<div className="App">
+				<CartContext.Provider value={{cart}}>
+					<Navigation />
+				</CartContext.Provider>
 
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
-
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
-		</div>
+				{/* Routes */}
+				<ProductContext.Provider value={{products, addItem}}>
+					<Route exact path="/">
+						<Products />
+					</Route>
+				</ProductContext.Provider>
+				<CartContext.Provider value={{cart, removeItem}}>
+					<Route path="/cart">
+						<ShoppingCart />
+					</Route>
+				</CartContext.Provider>
+			</div>
 	);
 }
 
